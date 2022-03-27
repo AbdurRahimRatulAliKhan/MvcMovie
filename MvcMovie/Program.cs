aa4a6bd9a5da0c15b8b543b3f7d483a5875a9d8a
Ratul Ali <rahim498617@gmail.com>
2022-03-27 16:54:36 +0600

    Added and Update model classes

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
