159619b4e72cef79209aef9f4fa83e4e22313021
Ratul Ali <rahim498617@gmail.com>
2022-03-27 18:10:22 +0600

    CRUD with Initial migration and generated database context class and registration

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -5,11 +5,11 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime? ReleaseDate { get; set; }
-        public string? Gnere { get; set; }
+        public string Gnere { get; set; }
         public decimal? Price { get; set; }
     }
 }
