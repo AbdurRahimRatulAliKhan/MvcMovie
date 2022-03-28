ff79fa4719c7273d613cfc00d2584bcde6969799
Ratul Ali <rahim498617@gmail.com>
2022-03-28 18:15:45 +0600

    Updated controller actions and views

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -7,9 +9,12 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime? ReleaseDate { get; set; }
         public string Gnere { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal? Price { get; set; }
     }
 }
